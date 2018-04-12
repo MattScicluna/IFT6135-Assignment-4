6b5568a48a93b3d2e933f26426e637312ca23a31
mattscicluna <mattcscicluna@gmail.com>
2018-04-12 15:32:40 -0400

    added training regime

---
+++ helpers.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import torch
+from torchvision import transforms
+import numpy as np
+
+
+def save_image_sample(batch, save_num, cuda, total_examples):
+    to_save = np.random.randint(batch.shape[0], size=save_num)
+    for ix, k in enumerate(to_save):
+        if cuda:
+            plt.imshow(transforms.ToPILImage()(batch[k].data.cpu()))
+            plt.savefig('results/generated_images/example-{}-sample-{}'.format(total_examples, ix))
+        else:
+            plt.imshow(transforms.ToPILImage()(batch[k]))
+            plt.savefig('results/generated_images/example-{}-sample-{}'.format(total_examples, ix))
+
+
+def save_checkpoint(total_examples, disc, gen, gen_losses, disc_losses):
+    basename = "models/example-{}".format(total_examples)
+    model_fname = basename + ".model"
+    state = {
+                'total_examples': total_examples,
+                'gen_state_dict': gen.state_dict(),
+                'disc_state_dict': disc.state_dict(),
+                'gen_losses': gen_losses,
+                'disc_losses': disc_losses
+            }
+    torch.save(state, model_fname)
--- train.py
+++ train.py
@@ -4,26 +4,24 @@ import os
 
 # thirdparty imports
 from torch.utils.data import DataLoader
-from torchvision import transforms, datasets
-import matplotlib.pyplot as plt
+from torchvision import datasets
+
 from torch.autograd import Variable
-import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
 
 # from project
 from model import Generator, Discriminator
-
-SEED = 1024
-
-
-def see_image(tensor):
-    assert isinstance(tensor, torch.FloatTensor)
-    plt.imshow(transforms.ToPILImage()(tensor))
+from helpers import *
 
 
-def main(train_set, learning_rate, n_epochs, batch_size, num_workers, hidden_size, model_file, cuda):
+def main(train_set, learning_rate, n_epochs, beta_0, beta_1, batch_size, num_workers, hidden_size,
+         model_file, cuda, display_result_every, checkpoint_interval, seed):
 
-    data_transform = transforms.Compose([transforms.ToTensor(), ])
+    #  make data between -1 and 1
+    data_transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
 
     train_dataset = datasets.ImageFolder(root=os.path.join(os.getcwd(), train_set),
                                          transform=data_transform)
@@ -35,21 +33,123 @@ def main(train_set, learning_rate, n_epochs, batch_size, num_workers, hidden_siz
     # initialize model
     if model_file == None:
         gen = Generator(hidden_dim=hidden_size)
-        disc = Discriminator()
+        disc = Discriminator(leaky=0.2)
 
-    np.random.seed(SEED)  # reset training seed to ensure that batches remain the same between runs!
-    for batch, _ in train_dataloader:
+        gen.weight_init(mean=0, std=0.02)
+        disc.weight_init(mean=0, std=0.02)
 
-        batch = Variable(batch)
-        #see_image(batch[0])
-        if cuda:
-            batch = batch.cuda()
+        total_examples = 0
+        disc_losses = []
+        gen_losses = []
 
-        # training regime
-        z = Variable(torch.zeros(32, 100))
-        gen = Generator(hidden_dim=100)
-        x = gen.forward(z.view(-1, 100, 1, 1))
-        disc.forward(x)
+    else:
+        from_before = torch.load(model_file)
+        total_examples = from_before['total_examples']
+        gen_losses = from_before['gen_losses']
+        disc_losses = from_before['disc_losses']
+        gen_state_dict = from_before['gen_state_dict']
+        disc_state_dict = from_before['disc_state_dict']
+
+        # load generator and discriminator
+        gen = Generator(hidden_dim=hidden_size)
+        disc = Discriminator(leaky=0.2)
+        disc.load_state_dict(disc_state_dict)
+        gen.load_state_dict(gen_state_dict)
+
+    if cuda:
+        gen.cuda()
+        disc.cuda()
+
+    # Binary Cross Entropy loss
+    BCE_loss = nn.BCELoss()
+
+    # Adam optimizer
+    gen_optimizer = optim.Adam(gen.parameters(), lr=learning_rate, betas=(beta_0, beta_1))
+    disc_optimizer = optim.Adam(disc.parameters(), lr=learning_rate, betas=(beta_0, beta_1))
+
+    # results save folder
+    if not os.path.isdir('results/generated_images'):
+        os.mkdir('results/generated_images')
+    if not os.path.isdir('results/training_summaries'):
+        os.mkdir('results/training_summaries')
+    if not os.path.isdir('models'):
+        os.mkdir('models')
+
+    np.random.seed(seed)  # reset training seed to ensure that batches remain the same between runs!
+
+    for epoch in range(n_epochs):
+        disc_losses_epoch = []
+        gen_losses_epoch = []
+        for idx, (true_batch, _) in enumerate(train_dataloader):
+            #  Sample  minibatch  of examples from data generating distribution
+            if cuda:
+                true_batch = Variable(true_batch.cuda())
+                true_target = Variable(torch.ones(batch_size).cuda())
+            else:
+                true_batch = Variable(true_batch)
+                true_target = Variable(torch.ones(batch_size))
+
+            #  Sample minibatch of m noise samples from noise prior p_g(z) and transform
+            if cuda:
+                z = Variable(torch.zeros(batch_size, hidden_size).cuda())
+                fake_target = Variable(torch.zeros(batch_size).cuda())
+            else:
+                z = Variable(torch.zeros(batch_size, hidden_size))
+                fake_target = Variable(torch.zeros(batch_size))
+
+            fake_batch = gen.forward(z.view(-1, hidden_size, 1, 1))
+
+            #  train discriminator
+            disc.zero_grad()
+            true_disc_result = disc.forward(true_batch)
+            fake_disc_result = disc.forward(fake_batch)
+
+            disc_train_loss = BCE_loss(true_disc_result.squeeze(), true_target) + \
+                              BCE_loss(fake_disc_result.squeeze(), fake_target)
+
+            disc_train_loss.backward()
+            disc_optimizer.step()
+            disc_losses_epoch.append(disc_train_loss.data[0])
+
+            #  Sample minibatch of m noise samples from noise prior p_g(z) and transform
+            if cuda:
+                z = Variable(torch.zeros(batch_size, hidden_size).cuda())
+            else:
+                z = Variable(torch.zeros(batch_size, hidden_size))
+
+            fake_batch = gen.forward(z.view(-1, hidden_size, 1, 1))
+
+            #  train generator
+            gen.zero_grad()
+            disc_result = disc.forward(fake_batch)
+            gen_train_loss = BCE_loss(disc_result.squeeze(), true_target)
+
+            gen_train_loss.backward()
+            gen_optimizer.step()
+            gen_losses_epoch.append(gen_train_loss.data[0])
+            if (idx+1) % display_result_every == 0:
+                print('epoch {}: step {}/{} disc loss: {:.4f}, gen loss: {:.4f}'
+                      .format(epoch+1, idx+1, len(train_dataloader), disc_train_loss.data[0], gen_train_loss.data[0]))
+
+            # Checkpoint model
+            total_examples += batch_size
+            if (checkpoint_interval != 0) and (total_examples % checkpoint_interval == 0):
+
+                disc_losses.extend(disc_losses_epoch)
+                gen_losses.extend(gen_losses_epoch)
+                save_checkpoint(total_examples, disc, gen, gen_losses, disc_losses)
+                print("Checkpoint saved!")
+
+                #  display generated image
+                save_image_sample(batch=fake_batch, save_num=5, cuda=cuda, total_examples=total_examples)
+                print("Saved images!")
+
+
+        print('epoch {}/{} disc loss: {:.4f}, gen loss: {:.4f}'
+              .format(epoch+1, n_epochs, np.array(disc_losses_epoch).mean(), np.array(gen_losses_epoch).mean()))
+
+        disc_losses.extend(disc_losses_epoch)
+        gen_losses.extend(gen_losses_epoch)
 
 
 if __name__ == '__main__':
@@ -57,17 +157,23 @@ if __name__ == '__main__':
     # Parse command line arguments
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--train_set', type=str, default='data/resized_celebA')
-    argparser.add_argument('--learning_rate', type=float, default=0.01)
+    argparser.add_argument('--learning_rate', type=float, default=0.0002)
     argparser.add_argument('--n_epochs', type=int, default=30)
-    argparser.add_argument('--batch_size', type=int, default=25)
+    argparser.add_argument('--batch_size', type=int, default=128)
+    argparser.add_argument('--beta_0', type=float, default=0.5)
+    argparser.add_argument('--beta_1', type=float, default=0.999)
     argparser.add_argument('--num_workers', type=int, default=8)
     argparser.add_argument('--hidden_size', type=int, default=100)
-    argparser.add_argument('--model_file', type=str, default='None')
-    argparser.add_argument('--cuda', action='store_true')
+    argparser.add_argument('--model_file', type=str, default=None)
+    argparser.add_argument('--cuda', action='store_true', default=True)
+    argparser.add_argument('--display_result_every', type=int, default=640)
+    argparser.add_argument('--checkpoint_interval', type=int, default=3200)
+    argparser.add_argument('--seed', type=int, default=1024)
     args = argparser.parse_args()
 
     args.cuda = args.cuda and torch.cuda.is_available()
 
     main(train_set=args.train_set, learning_rate=args.learning_rate, n_epochs=args.n_epochs,
-         batch_size=args.batch_size, num_workers=args.num_workers, hidden_size=args.hidden_size,
-         model_file=args.model_file, cuda=args.cuda)
+         beta_0=args.beta_0, beta_1=args.beta_1, batch_size=args.batch_size, num_workers=args.num_workers,
+         hidden_size=args.hidden_size, model_file=args.model_file, cuda=args.cuda,
+         display_result_every=args.display_result_every, checkpoint_interval=args.checkpoint_interval, seed=args.seed)
